dc4e13b1c617984b50a4d8e62b15b71b8de37513
Ian Patton <ian.patton@gmail.com>
2018-01-17 15:14:59 -0500

    cleanup: normalizing changes across libs

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
 'use strict';
 
-var bchLib = {};
 var owsCommon = require('@owstack/ows-common');
 
 // module information
-bchLib.version = 'v' + require('./package.json').version;
+module.exports.version = 'v' + require('./package.json').version;
 
 // crypto
-bchLib.crypto = {};
-bchLib.crypto.BN = owsCommon.crypto.BN;
-bchLib.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bchLib.crypto.Hash = owsCommon.crypto.Hash;
-bchLib.crypto.Random = owsCommon.crypto.Random;
-bchLib.crypto.Point = require('./lib/crypto/point');
-bchLib.crypto.Signature = require('./lib/crypto/signature');
+module.exports.crypto = {};
+module.exports.crypto.BN = owsCommon.crypto.BN;
+module.exports.crypto.ECDSA = require('./lib/crypto/ecdsa');
+module.exports.crypto.Hash = owsCommon.crypto.Hash;
+module.exports.crypto.Random = owsCommon.crypto.Random;
+module.exports.crypto.Point = require('./lib/crypto/point');
+module.exports.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-bchLib.encoding = {};
-bchLib.encoding.Base58 = owsCommon.encoding.Base58;
-bchLib.encoding.Base58Check = owsCommon.encoding.Base58Check;
-bchLib.encoding.BufferReader = owsCommon.encoding.BufferReader;
-bchLib.encoding.BufferWriter = owsCommon.encoding.BufferWriter;
-bchLib.encoding.Varint = owsCommon.encoding.Varint;
+module.exports.encoding = {};
+module.exports.encoding.Base58 = owsCommon.encoding.Base58;
+module.exports.encoding.Base58Check = owsCommon.encoding.Base58Check;
+module.exports.encoding.BufferReader = owsCommon.encoding.BufferReader;
+module.exports.encoding.BufferWriter = owsCommon.encoding.BufferWriter;
+module.exports.encoding.Varint = owsCommon.encoding.Varint;
 
 // utilities
-bchLib.util = {};
-bchLib.util.buffer = owsCommon.util.buffer;
-bchLib.util.js = owsCommon.util.js;
-bchLib.util.preconditions = owsCommon.util.preconditions;
+module.exports.util = {};
+module.exports.util.buffer = owsCommon.util.buffer;
+module.exports.util.js = owsCommon.util.js;
+module.exports.util.preconditions = owsCommon.util.preconditions;
 
 // errors thrown by the library
-bchLib.errors = owsCommon.errors;
+module.exports.errors = owsCommon.errors;
 
 // main bitcoin library
-bchLib.Address = require('./lib/address');
-bchLib.Block = require('./lib/block');
-bchLib.Constants = require('./lib/common/constants');
-bchLib.MerkleBlock = require('./lib/block/merkleblock');
-bchLib.BlockHeader = require('./lib/block/blockheader');
-bchLib.HDPrivateKey = require('./lib/hdprivatekey.js');
-bchLib.HDPublicKey = require('./lib/hdpublickey.js');
-bchLib.Networks = require('./lib/networks');
-bchLib.Opcode = require('./lib/opcode');
-bchLib.PrivateKey = require('./lib/privatekey');
-bchLib.PublicKey = require('./lib/publickey');
-bchLib.Script = require('./lib/script');
-bchLib.Transaction = require('./lib/transaction');
-bchLib.URI = require('./lib/uri');
-bchLib.Unit = require('./lib/unit');
+module.exports.Address = require('./lib/address');
+module.exports.Block = require('./lib/block');
+module.exports.Constants = require('./lib/common/constants');
+module.exports.MerkleBlock = require('./lib/block/merkleblock');
+module.exports.BlockHeader = require('./lib/block/blockheader');
+module.exports.HDPrivateKey = require('./lib/hdprivatekey.js');
+module.exports.HDPublicKey = require('./lib/hdpublickey.js');
+module.exports.Networks = require('./lib/networks');
+module.exports.Opcode = require('./lib/opcode');
+module.exports.PrivateKey = require('./lib/privatekey');
+module.exports.PublicKey = require('./lib/publickey');
+module.exports.Script = require('./lib/script');
+module.exports.Transaction = require('./lib/transaction');
+module.exports.URI = require('./lib/uri');
+module.exports.Unit = require('./lib/unit');
 
 // dependencies, subject to change
-bchLib.deps = {};
-bchLib.deps.bnjs = owsCommon.deps.bnjs;
-bchLib.deps.bs58 = owsCommon.deps.bs58;
-bchLib.deps.Buffer = owsCommon.deps.Buffer;
-bchLib.deps.elliptic = require('elliptic');
-bchLib.deps._ = owsCommon.deps._;
+module.exports.deps = {};
+module.exports.deps.bnjs = owsCommon.deps.bnjs;
+module.exports.deps.bs58 = owsCommon.deps.bs58;
+module.exports.deps.Buffer = owsCommon.deps.Buffer;
+module.exports.deps.elliptic = require('elliptic');
+module.exports.deps._ = owsCommon.deps._;
 
 // Internal usage, exposed for testing/advanced tweaking
-bchLib.Transaction.sighash = require('./lib/transaction/sighash');
-
-module.exports = bchLib;
+module.exports.Transaction.sighash = require('./lib/transaction/sighash');
